f9a58858ab6681309d391c968ea4f48feb61dd75
Steven Hertz <StevenH@appi4.com>
2024-04-17 17:51:10 -0400

    Initial Commit

---
+++ Consistent uiKit nav bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Consistent uiKit nav bar
+//
+//  Created by Steven Hertz on 4/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
